d82a2ddd127616b2272d50161707475595424da1
leomax2016 <leomax2016@mail.ru>
2019-03-13 02:04:28 +0400

    Add project files.

---
+++ ShoyanKursayin/Fill.cs
@@ -0,0 +1,469 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ShoyanKursayin
+{
+	class Fill
+	{
+		#region Fields
+		MainWindow context;
+		Dictionary<int, string> Rows = new Dictionary<int, string>();
+		Dictionary<int, int> RowsPriorities = new Dictionary<int, int>();
+		Dictionary<int, string> Topics = new Dictionary<int, string>();
+		Dictionary<int, int> RowID_TopicsID = new Dictionary<int, int>();
+		Dictionary<string, int> WordPriorities = new Dictionary<string, int>();
+		List<string> PrevAnswersList = new List<string>();
+		public List<string> PrevQuestionsList = new List<string>();
+		public int pos = 0;
+		public List<string> AllPrevQuestionList = new List<string>();
+		string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+		string StandardQuestion { get; set; } = "Хотите знать что-то еще?";
+		string AmbigousQuestion { get; set; } = "Что вас конкретно интересует по теме " + "\"";
+		string NoQuestion { get; set; } = "Я не знаю ответ на Ваш вопрос. Пожалуйста, переформулируйте его.";
+		string AlsoAnswered { get; set; } = "Я либо не знаю ответ на этот вопрос, либо уже отвечал на него!";
+		string LastQuestion { get; set; }
+		string StandardLongQuestion { get; set; } = " Хотите узнать по подробнее?";
+		string FullAnswer { get; set; }
+		int maxVal = 0, questionKey = 0, prevQuestionKey = 0;
+		bool isLongAnswer = false;
+		bool isAmbigousAnwer = true;
+		bool isConcreteAnswer = false;
+		#endregion Fields
+		#region Constructor
+		public Fill(MainWindow context)
+		{
+			this.context = context;
+			GetData();
+		}
+		#endregion Constructor
+		#region Methods
+		// Method for closing window
+		public void Close()
+		{
+			Thread t = new Thread(() =>
+			{
+				Thread.Sleep(2000);
+				context.Dispatcher.Invoke(() =>
+				{
+					context.Close();
+				});
+			});
+			t.Start();
+		}
+		// Clear old values for making new checks 
+		void Clear()
+		{
+			foreach (KeyValuePair<int, int> elem in RowsPriorities.ToList())
+			{
+				RowsPriorities[elem.Key] = 0;
+			}
+			maxVal = 0;
+			questionKey = 0;
+			//prevQuestionKey = 0;
+			isConcreteAnswer = false;
+			isAmbigousAnwer = false;
+		}
+		// Method that returns the final answer of our question
+		public string GetAnswer(string currentQuestion)
+		{
+			if (isLongAnswer)
+			{
+				isLongAnswer = false;
+				if(currentQuestion.ToLower() == "да")
+				{
+					return FullAnswer;
+				}
+				else if(currentQuestion.ToLower() == "нет")
+				{
+					LastQuestion = StandardQuestion;
+					return StandardQuestion;
+				}
+			}
+			if (LastQuestion == StandardQuestion)
+			{
+				string answer = String.Empty;
+				switch (currentQuestion)
+				{
+					case "да":
+						LastQuestion = null;
+						answer = "Чем я могу помочь?";
+						break;
+					case "нет":
+						LastQuestion = null;
+
+						answer = "До свидания!";
+						Close();
+						break;
+				}
+				if (answer != string.Empty)
+				{
+					return answer;
+				}
+			}
+			if (currentQuestion.ToLower() == "спасибо" || currentQuestion.ToLower() == "круто" || currentQuestion.ToLower() == "здорово")
+			{
+				LastQuestion = StandardQuestion;
+				return LastQuestion;
+			}
+			else
+			{
+				LastQuestion = null;
+			}
+			string temp = null;
+			RemoveRedundantWords(ref currentQuestion);
+			if (String.IsNullOrEmpty(currentQuestion))
+			{
+				return NoQuestion;
+			}
+			FindQuestion(currentQuestion);
+			GetAnswerType(currentQuestion);
+			if (isConcreteAnswer)
+			{
+				string ans = Answer(currentQuestion);
+				PrevQuestionsList.Add(currentQuestion);
+				Clear();
+				if (!PrevAnswersList.Contains(FindAnswerID(ans)))
+				{
+					PrevAnswersList.Add(FindAnswerID(ans));
+					if (isLongAnswer)
+					{
+						//isLongAnswer = false;
+						return ans + StandardLongQuestion;
+					}
+					return ans;
+				}
+				else
+				{
+					return AlsoAnswered;
+				}
+			}
+			else
+			{
+				for (int i = (PrevQuestionsList.Count - 1); i >= 0; i--)
+				{
+					List<string> qq = PrevQuestionsList[i].Split(' ').ToList<string>();
+					foreach (string el in Find(qq))
+					{
+						if (isAmbigousAnwer)
+						{
+							Clear();
+							prevQuestionKey = i;
+							temp = el + ' ' + currentQuestion;
+							FindQuestion(temp);
+							GetAnswerType(temp);
+						}
+						else
+						{
+							goto Home;
+						}
+					}
+
+
+				}
+			}
+			foreach (string el in Find(PrevQuestionsList))
+			{
+				if (isAmbigousAnwer)
+				{
+					Clear();
+					temp = el + ' ' + currentQuestion;
+					FindQuestion(temp);
+					GetAnswerType(temp);
+				}
+				else
+				{
+					goto Home;
+				}
+			}
+			Home:
+			PrevQuestionsList.Add(currentQuestion);
+			string topicName = GetTopic(questionKey);
+			if (isConcreteAnswer)
+			{
+				if (prevQuestionKey != 0)
+				{
+					PrevQuestionsList.RemoveRange(prevQuestionKey + 1, PrevQuestionsList.Count - prevQuestionKey - 1);
+					PrevQuestionsList.Add(currentQuestion);
+				}
+				string answer = Answer((temp));
+				Clear();
+				if (!PrevAnswersList.Contains(FindAnswerID(answer)))
+				{
+					PrevAnswersList.Add(answer);
+					if(isLongAnswer)
+					{
+						return answer + StandardLongQuestion;
+					}
+					else
+					{
+						return answer;
+					}
+				}
+				else
+				{
+					return AlsoAnswered;
+				}
+			}
+			else
+			{
+				Clear();
+				if (String.IsNullOrEmpty(topicName))
+				{
+					return AmbigousQuestion + currentQuestion + '\"';
+				}
+				return AmbigousQuestion + topicName + '\"';
+			}
+
+		}
+		//
+		string FindAnswerID(string ans)
+		{
+			using (SqlConnection conn = new SqlConnection(cs))
+			{
+				conn.Open();
+				StringBuilder st = new StringBuilder("Select Answers.Answer_ID FROM Answers WHERE AnswerText = @answer OR AlterAnswer1 = @answer OR AlterAnswer2 = @answer");
+				SqlCommand cmd = new SqlCommand(st.ToString(), conn);
+				SqlParameter paramID = new SqlParameter("@answer", ans);
+				cmd.Parameters.Add(paramID);
+				SqlDataReader dr2 = cmd.ExecuteReader();
+				Random r = new Random();
+				while (dr2.Read())
+				{
+					return dr2["Answer_ID"].ToString();
+				}
+			}
+			return null;
+		}
+		// Removes redundant words
+		void RemoveRedundantWords(ref string question)
+		{
+			question = Regex.Replace(question, "[,.?!]", string.Empty);
+			List<string> words = question.ToLower().Split(' ').ToList();
+			List<string> oddWords = new List<string>();
+			//string[] words = question.ToLower().Split(' ').ToList();
+			foreach (string elem in words)
+			{
+				foreach (KeyValuePair<string, int> kv in WordPriorities)
+				{
+					if (elem.Contains(kv.Key))
+					{
+						goto next;
+					}
+				}
+				//List<string> l = words.ToList<string>();
+				oddWords.Add(elem);
+				//l.RemoveAt(Array.IndexOf(words, elem));
+				next:;
+			}
+			foreach (string elem in oddWords)
+			{
+				words.Remove(elem);
+			}
+			question = String.Join(" ", words);
+		}
+		// Method that returns the answer of the question
+		string Answer(string s)
+		{
+			using (SqlConnection conn = new SqlConnection(cs))
+			{
+				List<string> answers = new List<string>();
+				conn.Open();
+				StringBuilder st2 = new StringBuilder("Select Answers.AnswerText, Answers.AlterAnswer1, Answers.AlterAnswer2, Answers.FullAnswer From Questions Left Join Answers ON Answers.Answer_ID = Questions.Answer_ID WHERE Questions.Question_ID = ");
+				st2.Append(questionKey);
+				SqlCommand cmd2 = new SqlCommand(st2.ToString(), conn);
+				SqlDataReader dr2 = cmd2.ExecuteReader();
+				Random r = new Random();
+				while (dr2.Read())
+				{
+					answers.Add(dr2["AnswerText"].ToString());
+					if (!String.IsNullOrEmpty(dr2["FullAnswer"].ToString()))
+					{
+						isLongAnswer = true;
+						FullAnswer = dr2["FullAnswer"].ToString();
+					}
+					if (!String.IsNullOrEmpty(dr2["AlterAnswer1"].ToString()))
+					{
+						answers.Add(dr2["AlterAnswer1"].ToString());
+					}
+					if (!String.IsNullOrEmpty(dr2["AlterAnswer2"].ToString()))
+					{
+						answers.Add(dr2["AlterAnswer2"].ToString());
+					}
+					Clear();
+					
+						return answers[r.Next(0, answers.Count)];
+					
+				}
+			}
+			return null;
+		}
+		// Method that returns the type of the answer (concrete of ambigious)
+		void GetAnswerType(string s)
+		{
+			foreach (KeyValuePair<int, int> elem1 in RowsPriorities)
+			{
+				foreach (KeyValuePair<int, int> elem2 in RowsPriorities)
+				{
+					if (elem1.Value == elem2.Value && elem1.Key != elem2.Key && elem1.Value == maxVal && maxVal != 0)
+					{
+						LastQuestion += " " + s;
+						isConcreteAnswer = false;
+						isAmbigousAnwer = true;
+						return;
+					}
+				}
+			}
+
+			if (maxVal == 0)
+			{
+				isConcreteAnswer = false;
+				isAmbigousAnwer = false;
+			}
+			else
+			{
+				isConcreteAnswer = true;
+				isAmbigousAnwer = false;
+			}
+		}
+		// Method that checks whether our answer has a topic or no
+		string GetTopic(int questionKey)
+		{
+			if (RowID_TopicsID.Keys.Contains(questionKey) && Topics.Keys.Contains(RowID_TopicsID[questionKey]))
+			{
+				return Topics[RowID_TopicsID[questionKey]];
+			}
+			return null;
+		}
+		// Method that is called in constructor
+		// It collects all data from our database
+		void GetData()
+		{
+			using (SqlConnection conn = new SqlConnection(cs))
+			{
+				conn.Open();
+
+				string query1 = "SELECT Question_ID, QuestionText FROM Questions";
+				SqlCommand cmd1 = new SqlCommand(query1, conn);
+				SqlDataReader dr1 = cmd1.ExecuteReader();
+				string s;
+				while (dr1.Read())
+				{
+					s = (string)dr1["QuestionText"];
+					Rows.Add((int)dr1["Question_ID"], s.ToLower());
+				}
+				dr1.Close();
+				string query2 = "SELECT Word, Priority FROM WordsPriorities";
+				SqlCommand cmd2 = new SqlCommand(query2, conn);
+				SqlDataReader dr2 = cmd2.ExecuteReader();
+				while (dr2.Read())
+				{
+					s = (string)dr2["Word"];
+					WordPriorities.Add(s.ToLower(), Convert.ToInt32(dr2["Priority"]));
+				}
+				dr2.Close();
+				foreach (KeyValuePair<int, string> keyVal in Rows)
+				{
+					string[] QTextArr = keyVal.Value.Split(' ');
+
+					RowsPriorities.Add(keyVal.Key, 0);
+				}
+
+				string query3 = "SELECT * FROM Topics";
+				SqlCommand cmd3 = new SqlCommand(query3, conn);
+				SqlDataReader dr3 = cmd3.ExecuteReader();
+				while (dr3.Read())
+				{
+					Topics.Add(Convert.ToInt32(dr3["ID"]), Convert.ToString(dr3["Topic"]));
+				}
+				dr3.Close();
+
+				string query4 = "SELECT Question_ID, Topic_ID FROM Questions";
+				SqlCommand cmd4 = new SqlCommand(query4, conn);
+				SqlDataReader dr4 = cmd4.ExecuteReader();
+				while (dr4.Read())
+				{
+					if (dr4["Topic_ID"] != DBNull.Value)
+					{
+						RowID_TopicsID.Add(Convert.ToInt32(dr4["Question_ID"]), Convert.ToInt32(dr4["Topic_ID"]));
+					}
+				}
+				dr3.Close();
+			}
+		}
+		// Method that returns all possible questions from list
+		static IEnumerable<string> Find(List<string> str)
+		{
+			for (int i = str.Count - 1; i >= 0; i--)
+			{
+				for (var j = 1; j < str.Count - i + 1; j++)
+				{
+					string temp = "";
+					foreach (string k in str.GetRange(i, j))
+					{
+						temp += k + " ";
+					}
+					yield return temp;
+				}
+			}
+		}
+		// Method that accepts the question and trying to understand which question from db is thet
+		void FindQuestion(string question)
+		{
+			question = Regex.Replace(question, "[,.?!]", string.Empty);
+
+			string[] currentQuestionArr = question.ToLower().Split(' ');
+			Dictionary<int, List<string>> usedWords = new Dictionary<int, List<string>>();
+
+			foreach (string elem in currentQuestionArr)
+			{
+				if (elem.Length > 1)
+				{
+					foreach (KeyValuePair<int, string> keyVal in Rows)
+					{
+						if (!usedWords.ContainsKey(keyVal.Key))
+						{
+							usedWords.Add(keyVal.Key, new List<string>());
+						}
+						string[] QTextArr = keyVal.Value.Split(' ');
+						foreach (string QTextWord in QTextArr)
+						{
+							if (elem.Contains(QTextWord) && !usedWords[keyVal.Key].Contains(QTextWord))
+							{
+								usedWords[keyVal.Key].Add(elem);
+								foreach (KeyValuePair<string, int> wordPrior in WordPriorities)
+								{
+									if (elem.Contains(wordPrior.Key))
+									{
+										RowsPriorities[keyVal.Key] += wordPrior.Value;
+										break;
+									}
+								}
+							}
+
+						}
+						//usedWords.Clear();
+					}
+				}
+			}
+			foreach (KeyValuePair<int, int> elem in RowsPriorities)
+			{
+				if (elem.Value > maxVal)
+				{
+					maxVal = elem.Value;
+					questionKey = elem.Key;
+				}
+			}
+		}
+		#endregion Methods
+	}
+}
+
---
+++ ShoyanKursayin/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml;
+
+namespace ShoyanKursayin
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		Fill inst;
+
+		public MainWindow()
+		{
+
+			InitializeComponent();
+		}
+
+		
+		string ans;
+		string questio;
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			inst = new Fill(this);
+			questionBox.Focus();
+			XmlDocument doc = new XmlDocument();
+			doc.Load("logsHistory.xml");
+			XmlElement conv = doc.CreateElement("Conversation");
+			conv.SetAttribute("date", DateTime.Now.ToLongDateString());
+			conv.SetAttribute("time", DateTime.Now.ToLongTimeString());
+			XmlNode root = doc.ChildNodes.Item(1);
+			root.AppendChild(conv);
+			doc.Save("logsHistory.xml");
+		}
+		private void FillIsTypeing(object answer)
+		{
+			this.Dispatcher.Invoke(() =>
+			{
+				ans = inst.GetAnswer(answer.ToString());
+			});
+				Stopwatch stopwatch = new Stopwatch();
+			int time = Convert.ToInt32(ans.Length * 30);
+			for (int i = 0; i < 5; i++)
+			{
+				stopwatch.Restart();
+			do
+			{
+				this.Dispatcher.Invoke(() =>
+				{
+					
+						answerBlock.Text = "Филл Печатает";
+
+						if (stopwatch.ElapsedMilliseconds > 100)
+						{
+							answerBlock.Text = "Филл Печатает";
+						}
+						if (stopwatch.ElapsedMilliseconds > 300)
+						{
+							answerBlock.Text = "Филл Печатает.";
+						}
+						if (stopwatch.ElapsedMilliseconds > 500)
+						{
+							answerBlock.Text = "Филл Печатает..";
+						}
+						if (stopwatch.ElapsedMilliseconds > 700)
+						{
+							answerBlock.Text = "Филл Печатает...";
+						}
+					
+					
+				});
+			} while (stopwatch.ElapsedMilliseconds <= time);
+			}
+			stopwatch.Stop();
+			this.Dispatcher.Invoke(() =>
+			{
+				//answerBlock2.Text = String.Empty;
+				hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, ans);
+				hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, new string(' ', 10));
+				hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, "\n");
+				//answerBlock2.Text = hystoryTextBlock.Text.Insert(0, questionBox.Text);
+				hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, answer.ToString());
+				hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, "\n");
+				answerBlock.Text = ans;
+				questionBox.IsEnabled = true;
+				questionBox.Focus();
+			});
+		}
+		int count;
+		System.Timers.Timer timer = new System.Timers.Timer();
+
+		private void TextBox1_PrevKeyDown(object sender, KeyEventArgs e)
+		{
+			
+			if (e.Key == Key.Return)
+			{
+				
+				if (questionBox.Text.ToLower() == "пока" || questionBox.Text.ToLower() == "прощай")
+				{
+					MainWindow context = this;
+					inst.Close();
+
+					answerBlock.Text = "Прощай";
+				}
+				else if (questionBox.Text.Length > 0)
+				{
+					questionBox.IsEnabled = false;
+					inst.AllPrevQuestionList.Add(questionBox.Text);
+					inst.pos = inst.AllPrevQuestionList.Count;
+					timer = new System.Timers.Timer();
+					questio = questionBox.Text;
+					ans = inst.GetAnswer(questionBox.Text);
+					count = Convert.ToInt32(ans.Length / 3);
+					if(count > 30)
+					{
+						count = 20;
+					}
+					answerBlock.Text = "Филл Печатает";
+					timer.Interval = 200;
+					timer.Elapsed += new ElapsedEventHandler(Timer_Tick);
+					timer.Enabled = true;
+					timer.Start();
+					
+					#region XMLWrite
+					XmlDocument doc = new XmlDocument();
+					doc.Load("logsHistory.xml");
+					XmlElement rep = doc.CreateElement("Replica");
+					XmlElement quest = doc.CreateElement("Question");
+					XmlElement answ = doc.CreateElement("Answer");
+					quest.InnerText = questionBox.Text;
+					answ.InnerText = answerBlock.Text;
+					rep.AppendChild(quest);
+					rep.AppendChild(answ);
+					XmlNodeList convs = doc.GetElementsByTagName("Conversation");
+					XmlNode last = convs.Item(convs.Count - 1);
+					last.AppendChild(rep);
+					doc.Save("logsHistory.xml");
+					#endregion
+			
+					questionBox.Text = String.Empty;
+				}
+
+			}
+			else if (e.Key == Key.Up)
+			{
+				if (inst.pos > 0)
+				{
+					inst.pos--;
+					questionBox.Text = inst.AllPrevQuestionList[inst.pos];
+				}
+
+			}
+			else if (e.Key == Key.Down)
+			{
+				if (inst.pos < inst.AllPrevQuestionList.Count - 1)
+				{
+					inst.pos++;
+					questionBox.Text = inst.AllPrevQuestionList[inst.pos];
+				}
+				else
+				{
+					inst.pos = inst.AllPrevQuestionList.Count;
+					questionBox.Text =String.Empty;
+				}
+			}
+		}
+		void Timer_Tick(object sender, EventArgs e)
+		{
+			this.Dispatcher.Invoke(() =>
+			{
+				count--;
+				if (count == 0)
+				{
+					hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, ans);
+					hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, new string(' ', 10));
+					hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, "\n");
+					//answerBlock2.Text = hystoryTextBlock.Text.Insert(0, questionBox.Text);
+					hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, questio);
+					hystoryTextBlock.Text = hystoryTextBlock.Text.Insert(0, "\n");
+					answerBlock.Text = ans;
+					questionBox.IsEnabled = true;
+					questionBox.Focus();
+					timer.Stop();
+				}
+				else
+				{
+					if (answerBlock.Text == "Филл Печатает...")
+					{
+						answerBlock.Text = "Филл Печатает";
+					}
+					else
+					{
+						answerBlock.Text += '.';
+					}
+					// Do something here
+				}
+			});
+			//this.Dispatcher.Invoke(() =>
+			//{
+			//	answerBlock.Text += "a";
+			//});
+		}
+	}
+}
